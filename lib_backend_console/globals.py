c3a693f007e4fb65f3c643ce67f682bda731a6d8
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-11-24 08:25:59 -0500

    remove files for gitignore

--- lib_backend_console/globals.py
+++
@@ -1,7 +0,0 @@
-from asyncio import Queue
-from typing import Sequence, Dict
-
-
-class BackendConsoleGlobals:
-    queue: Queue = Queue()
-    console_content: Sequence[Dict] = []
--- lib_backend_console/ui.py
+++
@@ -1,5 +0,0 @@
-import gradio as gr
-
-
-def create_console_ui():
-    return gr.Code(label='', elem_classes=['backend-console-textbox'], interactive=True)
